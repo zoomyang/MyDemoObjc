95f2e0b81052f407577b3d35c34543d662820ea6
Jarek <jarekyang@163.com>
2016-10-12 20:35:21 +0800

    加入了CorePlot库 CorePlot库用户绘制折线图，柱状图

---
+++ MyDemoObjc/MyDemoObjc/MyTestView.h
@@ -0,0 +1,15 @@
+//
+//  MyTestView.h
+//  MyDemoObjc
+//
+//  Created by Yang Jian on 16/10/12.
+//  Copyright © 2016年 zdksii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import <CorePlot.h>
+@interface MyTestView : CPTGraphHostingView
+
+@end
---
+++ MyDemoObjc/MyDemoObjc/MyTestView.m
@@ -0,0 +1,21 @@
+//
+//  MyTestView.m
+//  MyDemoObjc
+//
+//  Created by Yang Jian on 16/10/12.
+//  Copyright © 2016年 zdksii. All rights reserved.
+//
+
+#import "MyTestView.h"
+
+@implementation MyTestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
